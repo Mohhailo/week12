fdfed83b6af4fb59e43f068367e5eb1ae276cb60
Mõhhailo Riško <misha@woodman.ee>
2024-12-11 22:07:05 +0200

    add getmovie rout

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const axios = require("axios");
-const http = require("http")
+const http = require("http");
 const app = express();
 
 app.set("view engine", "ejs");
